6dc2e9c0b65ee62e90b2cacf541f07dd76175625
LeonidLindBlom <leonid.galyba@mail.ru>
2022-11-29 16:21:53 +0600

    Задание решено

--- Login/src/com/company/Main.java
+++ Login/src/com/company/Main.java
@@ -1,8 +1,18 @@
 package com.company;
 
+import java.util.Scanner;
+
 public class Main {
 
-    public static void main(String[] args) {
-	// write your code here
+    public static void main(String[] args){
+        String regex ="[\\w]{1,20}";
+        Scanner loginScanner = new Scanner(System.in);
+        Scanner passwordScanner = new Scanner(System.in);
+        String login = loginScanner.nextLine();
+        String password = passwordScanner.nextLine();
+        String confirmPassword = passwordScanner.nextLine();
+        Login log = new Login();
+        Boolean a = log.checkCorrect(login, password,confirmPassword, regex);
+        System.out.println(a);
     }
 }
